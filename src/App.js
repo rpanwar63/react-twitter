b1f0697b18a49790b6f9096c99cd02af8a6aa5e7
Rishabh Panwar <rishabh.rp.rp@gmail.com>
2022-05-02 23:13:07 +0530

    initialCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  const login = async () => {
+    
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "OAuth oauth_consumer_key=\"GWwpLvoDJ7TTbRyQOtzvfygoF\",oauth_token=\"1375052412-YPyHbBelqLnrhhoiJqxwIztDzzMoTFfLIaWGd0a\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1651512120\",oauth_nonce=\"N8lwkT6c5Rl\",oauth_version=\"1.0\",oauth_signature=\"6jf72RLIp8JyBK9TakZIjPUQ%2BDo%3D\"");
+    // myHeaders.append("Cookie", "guest_id=v1%3A165151133627434406; lang=en");
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    fetch("https://api.twitter.com/oauth/request_token", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
+  }
+  useEffect(() => {
+    login()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
