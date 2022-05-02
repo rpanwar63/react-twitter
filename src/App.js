f2fc7874d9f66dbdbc7d1472d689104c878c4dea
Rishabh Panwar <rishabh.rp.rp@gmail.com>
2022-05-03 00:33:29 +0530

    initailCommit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,112 @@
-import './App.css';
-import { useEffect } from 'react';
+import React from "react";
+
+import "./styles.css";
+import { useInitFbSDK } from "./fb-hooks";
+
+// You can find your Page ID
+// in the "About" section of your page on Facebook.
+const PAGE_ID = "100165859364792";
 
 function App() {
-  const login = async () => {
-    
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "OAuth oauth_consumer_key=\"GWwpLvoDJ7TTbRyQOtzvfygoF\",oauth_token=\"1375052412-YPyHbBelqLnrhhoiJqxwIztDzzMoTFfLIaWGd0a\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1651512120\",oauth_nonce=\"N8lwkT6c5Rl\",oauth_version=\"1.0\",oauth_signature=\"6jf72RLIp8JyBK9TakZIjPUQ%2BDo%3D\"");
-    // myHeaders.append("Cookie", "guest_id=v1%3A165151133627434406; lang=en");
+  // Initializes the Facebook SDK
+  const isFbSDKInitialized = useInitFbSDK();
 
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
+  // App state
+  const [fbUserAccessToken, setFbUserAccessToken] = React.useState();
+  const [fbPageAccessToken, setFbPageAccessToken] = React.useState();
+  const [postText, setPostText] = React.useState();
+  const [isPublishing, setIsPublishing] = React.useState(false);
 
-    fetch("https://api.twitter.com/oauth/request_token", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
+  // Logs in a Facebook user
+  const logInToFB = React.useCallback(() => {
+    window.FB.login((response) => {
+      setFbUserAccessToken(response.authResponse.accessToken);
+    });
+  }, []);
 
-  }
-  useEffect(() => {
-    login()
-  },[])
+  // Logs out the current Facebook user
+  const logOutOfFB = React.useCallback(() => {
+    window.FB.logout(() => {
+      setFbUserAccessToken(null);
+      setFbPageAccessToken(null);
+    });
+  }, []);
 
+  // Checks if the user is logged in to Facebook
+  React.useEffect(() => {
+    if (isFbSDKInitialized) {
+      window.FB.getLoginStatus((response) => {
+        setFbUserAccessToken(response.authResponse?.accessToken);
+      });
+    }
+  }, [isFbSDKInitialized]);
+
+  // Fetches an access token for the page
+  React.useEffect(() => {
+    if (fbUserAccessToken) {
+      window.FB.api(
+        `/${PAGE_ID}?fields=access_token&access_token=${fbUserAccessToken}`,
+        ({ access_token }) => setFbPageAccessToken(access_token)
+      );
+    }
+  }, [fbUserAccessToken]);
+
+  // Publishes a post on the Facebook page
+  const sendPostToPage = React.useCallback(() => {
+    setIsPublishing(true);
+
+    window.FB.api(
+      `/${PAGE_ID}/feed`,
+      "POST",
+      {
+        message: postText,
+        access_token: fbPageAccessToken,
+      },
+      () => {
+        setPostText("");
+        setIsPublishing(false);
+      }
+    );
+  }, [postText, fbPageAccessToken]);
+
+  // UI with custom styling from ./styles.css`
   return (
-    <div className="App">
-      
+    <div id="app">
+      <header id="app-header">
+        <p id="logo-text">FB Page API</p>
+        {fbUserAccessToken ? (
+          <button onClick={logOutOfFB} className="btn confirm-btn">
+            Log out
+          </button>
+        ) : (
+          <button onClick={logInToFB} className="btn confirm-btn">
+            Login with Facebook
+          </button>
+        )}
+      </header>
+      <main id="app-main">
+        {fbPageAccessToken ? (
+          <section className="app-section">
+            <h3>Write something to the page</h3>
+            <textarea
+              value={postText}
+              onChange={(e) => setPostText(e.target.value)}
+              placeholder="Message..."
+              rows="8"
+              disabled={isPublishing}
+            />
+            <button
+              onClick={sendPostToPage}
+              className="btn confirm-btn"
+              disabled={!postText || isPublishing}
+            >
+              {isPublishing ? "Publishing..." : "Publish"}
+            </button>
+          </section>
+        ) : (
+          <h2 className="placeholder-container">Welcome!</h2>
+        )}
+      </main>
     </div>
   );
 }
